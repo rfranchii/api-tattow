d465e3bbb9abdb4dca8015d7c1df2974eb4f7474
Rodrigo Franchi <rfranchi95@gmail.com>
2024-07-10 21:40:07 -0300

    Commit inicial

---
+++ src/main/java/com/sqav/tattow/TattowApplication.java
@@ -0,0 +1,13 @@
+package com.sqav.tattow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqav/tattow/TattowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqav.tattow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
